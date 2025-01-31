8aabd7f16edbdd57fc189e9ed8dc9004bfc01f09
MoisesArr <96744545+MoisesArr@users.noreply.github.com>
2025-01-30 21:53:17 -0600

    Set up Profile page as well as fixed up home page

--- contexts/authContext.tsx
+++ contexts/authContext.tsx
@@ -2,9 +2,9 @@ import { auth, firestore } from "@/config/firebase";
 import { AuthContextType, UserType } from "@/types";
 import { useRouter } from "expo-router";
 import {
-    createUserWithEmailAndPassword, 
-    onAuthStateChanged, 
-    signInWithEmailAndPassword 
+    createUserWithEmailAndPassword,
+    onAuthStateChanged,
+    signInWithEmailAndPassword
 } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { createContext, useContext, useEffect, useState } from "react";
@@ -14,103 +14,103 @@ const AuthContext = createContext<AuthContextType | null>(null);
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
     children,
 }) => {
-  const [user, setUser] = useState<UserType>(null);
-  const router = useRouter();
+    const [user, setUser] = useState<UserType>(null);
+    const router = useRouter();
 
-  useEffect(()=>{
-    const unsub = onAuthStateChanged(auth, (fireBaseUser) => {
-        console.log('firebase user: ', fireBaseUser);
-        if(fireBaseUser){
-            setUser({
-                uid: fireBaseUser?.uid,
-                email: fireBaseUser?.email,
-                name: fireBaseUser?.displayName
-            });
-            updateUserData(fireBaseUser.uid)
-            router.replace("/(tabs)")
-        }else{
-            // no user
-            setUser(null);
-            router.replace("/(auth)/welcome");
-        }
-    });
-
-    return() => unsub();
-}, []);
-
-  const login = async (email: string, password: string) => {
-    try {
-        await signInWithEmailAndPassword (auth, email, password);
-        return { success: true };
-    } catch (error: any) {
-        let msg = error.message;
-        console.log("error message: ", msg)
-        if(msg.includes('(auth/invalid-credential)')) msg = "Wrong credentials";
-        if(msg.includes('(auth/invalid-email)')) msg = "Invalid email";
-        return { success: false, msg };
-    }
-  };
-  const register = async (email: string, password: string, name: string) => {
-    try {
-        let response = await  createUserWithEmailAndPassword(
-            auth, 
-            email, 
-            password
-        );
-        await setDoc(doc(firestore, "users", response?.user?.uid), {
-            name,
-            email,
-            uid: response?.user?.uid,
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (fireBaseUser) => {
+            console.log('firebase user: ', fireBaseUser);
+            if (fireBaseUser) {
+                setUser({
+                    uid: fireBaseUser?.uid,
+                    email: fireBaseUser?.email,
+                    name: fireBaseUser?.displayName
+                });
+                updateUserData(fireBaseUser.uid)
+                router.replace("/(tabs)")
+            } else {
+                // no user
+                setUser(null);
+                router.replace("/(auth)/welcome");
+            }
         });
-        return { success: true };
-    } catch (error: any) {
-        let msg = error.message;
-        console.log("error message: ", msg)
-        if(msg.includes('(auth/email-already-in-use)')) msg = "This email is already in use";
-        if(msg.includes('(auth/invalid-email)')) msg = "Invalid email";
 
-        return { success: false, msg };
-    }
-  };
+        return () => unsub();
+    }, []);
 
-  const updateUserData = async (uid: string) =>{
-    try {
-        const docRef = doc(firestore, "users", uid);
-        const docSnap = await getDoc(docRef);
-
-        if(docSnap.exists()){
-            const data = docSnap.data();
-            const userData: UserType = {
-                uid: data?.uid,
-                email: data.email || null,
-                name: data.name || null,
-                image: data.image || null,
-            };
-            setUser({ ...userData});
+    const login = async (email: string, password: string) => {
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            return { success: true };
+        } catch (error: any) {
+            let msg = error.message;
+            console.log("error message: ", msg)
+            if (msg.includes('(auth/invalid-credential)')) msg = "Wrong credentials";
+            if (msg.includes('(auth/invalid-email)')) msg = "Invalid email";
+            return { success: false, msg };
         }
-    } catch (error: any) {
-        let msg = error.message;
-        // return { success: false, msg };
-        console.log("error: ", error);
-    }
-  };
+    };
+    const register = async (email: string, password: string, name: string) => {
+        try {
+            let response = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            await setDoc(doc(firestore, "users", response?.user?.uid), {
+                name,
+                email,
+                uid: response?.user?.uid,
+            });
+            return { success: true };
+        } catch (error: any) {
+            let msg = error.message;
+            console.log("error message: ", msg)
+            if (msg.includes('(auth/email-already-in-use)')) msg = "This email is already in use";
+            if (msg.includes('(auth/invalid-email)')) msg = "Invalid email";
 
-  const contextValue: AuthContextType = {
-    user,
-    setUser,
-    login,
-    register,
-    updateUserData
-  };
-  
-  return (
-    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
-  );
+            return { success: false, msg };
+        }
+    };
+
+    const updateUserData = async (uid: string) => {
+        try {
+            const docRef = doc(firestore, "users", uid);
+            const docSnap = await getDoc(docRef);
+
+            if (docSnap.exists()) {
+                const data = docSnap.data();
+                const userData: UserType = {
+                    uid: data?.uid,
+                    email: data.email || null,
+                    name: data.name || null,
+                    image: data.image || null,
+                };
+                setUser({ ...userData });
+            }
+        } catch (error: any) {
+            let msg = error.message;
+            // return { success: false, msg };
+            console.log("error: ", error);
+        }
+    };
+
+    const contextValue: AuthContextType = {
+        user,
+        setUser,
+        login,
+        register,
+        updateUserData
+    };
+
+    return (
+        <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+    );
 };
 
-export const useAuth = ():AuthContextType => {
+export const useAuth = (): AuthContextType => {
     const context = useContext(AuthContext);
-    if(!context) {
+    if (!context) {
         throw new Error("useAuth must be wrapped inside AuthProvider");
     }
     return context;
